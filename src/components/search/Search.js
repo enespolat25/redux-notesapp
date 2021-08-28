974119ce8dafff5b8e0bdc9950219612127bab69
hakanolgun <kaptanhakanolgun@gmail.com>
2021-08-28 12:40:14 +0300

    search function completed

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from "react";
 import styles from "./styles.module.css";
+import { setSearchValue } from "../../redux/notes/notesSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function Search() {
-    return (
-        <div>
-            <input className={styles.searchinput} placeholder="Search..." type="text" />
-        </div>
-    )
+  const dispatch = useDispatch();
+  const searchValue = useSelector((state) => state.notes.searchValue);
+
+  const handleSearch = (targetvalue) => {
+    dispatch(setSearchValue(targetvalue));
+  };
+
+  return (
+    <div>
+      <input
+        className={styles.searchinput}
+        placeholder="Search..."
+        type="text"
+        value={searchValue}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+    </div>
+  );
 }
 
-export default Search
+export default Search;
